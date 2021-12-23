7a62afa37540fd423ab4cc814ad6dcd3dd23c59f
NickDiSanto <nicolasc.disanto@calbaptist.edu>
2021-12-22 17:22:50 -0800

    Began implementation of Bishop

--- src/Bishop.java
+++ src/Bishop.java
@@ -5,4 +5,28 @@ public class Bishop extends Piece {
     public Bishop(int coordinate, boolean isWhite, char pieceType, LinkedList<Piece> pieces) {
         super(coordinate, isWhite, pieceType, pieces);
     }
+
+    @Override
+    public void move(int coordinate) {
+        if ((coordinate - this.coordinate % 9 == 0 || coordinate - this.coordinate % 11 == 0) && // TODO: only allow movement if there are no pieces inbetween)
+            if (Board.getPiece(coordinate / 10 * 64, coordinate % 10 * 64) != null) {
+                if (Board.getPiece(coordinate / 10 * 64, coordinate % 10 * 64).isWhite != isWhite)
+                    Board.getPiece(coordinate / 10 * 64, coordinate % 10 * 64).capture();
+        }
+    }
+
+//    public void move (int coordinate) {
+//        if (Board.getPiece(coordinate / 10 * 64, coordinate % 10 * 64) != null) {
+//            if (Board.getPiece(coordinate / 10 * 64, coordinate % 10 * 64).isWhite != isWhite)
+//                Board.getPiece(coordinate / 10 * 64, coordinate % 10 * 64).capture();
+//            else {
+//                xPixel = this.coordinate / 10 * 64;
+//                yPixel = this.coordinate % 10 * 64;
+//                return;
+//            }
+//        }
+//        this.coordinate = coordinate;
+//        xPixel = coordinate / 10 * 64;
+//        yPixel = coordinate % 10 * 64;
+//    }
 }
